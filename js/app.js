b07f531db4240dec686fd95aed5b7bdce5c549c7
Anthony Curtis <tonydc1997@gmail.com>
2018-03-29 07:59:48 -0400

    Finish blog previewer functionality

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
-$('#flashMessage')
-.hide()
+$('#flashMessage').hide();
+
+$('#previewButton').click( () => {
+  const title = $('#blogTitleInput').val();
+  const content = $('#blogContentInput').val();
+
+  $('#blogTitlePreview').text(title);
+  $('#blogContentPreview').html(content);
+
+  $('#flashMessage')
   .slideDown()
   .delay(3000)
   .slideUp();
-
-const title = "My First Blog Post";
-const content = "This is my <strong>first</strong> post!";
-
-$('#blogTitlePreview').text(title);
-$('#blogContentPreview').html(content);
+});
